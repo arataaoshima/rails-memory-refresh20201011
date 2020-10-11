939d6e3da075369488b4078ca32da15702b7b929
Arata <newbluelight1031@gmail.com>
2020-10-11 12:26:50 -0700

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  devise_for :users, controllers: {
+  omniauth_callbacks: "users/omniauth_callbacks"
+ }
+  root 'home#top'
+  get 'home/about'
+  get 'home/contact'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
