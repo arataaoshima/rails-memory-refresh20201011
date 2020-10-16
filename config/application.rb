57eb183201e4fdfe2c93f1e4ab0f6dd9b56a0b2d
Arata <newbluelight1031@gmail.com>
2020-10-16 00:32:59 -0700

    preparation for Heroku

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module CsvEmbedTest
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.assets.initialize_on_precompile = false
     config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag }
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
