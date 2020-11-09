0d81aec8ff9c9fe1dd7ba1e59918657316ab0383
Arata <newbluelight1031@gmail.com>
2020-11-09 14:51:07 -0800

    delete duplicated purchased_at

--- db/migrate/20201026043038_change_times_to_users.rb
+++ db/migrate/20201026043038_change_times_to_users.rb
@@ -4,7 +4,6 @@ class ChangeTimesToUsers < ActiveRecord::Migration[6.0]
     add_column :users, :current_sign_in_at, :datetime
     remove_column :users, :last_sign_in_at
     add_column :users, :last_sign_in_at, :datetime
-    add_column :users, :purchased_at, :time
     add_column:users, :sub_start, :string
   end
 end
