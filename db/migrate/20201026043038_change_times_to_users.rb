1d20b39e53dfda87dec220f1d42b9f43dae08d60
Arata <newbluelight1031@gmail.com>
2020-11-09 14:36:40 -0800

    fix Migration to change_colomn gives error cannot be cast to type timestamp without time zone

--- db/migrate/20201026043038_change_times_to_users.rb
+++ db/migrate/20201026043038_change_times_to_users.rb
@@ -1,8 +1,11 @@
 class ChangeTimesToUsers < ActiveRecord::Migration[6.0]
   def change
-    change_column :users, :current_sign_in_at, :timestamp
-    change_column :users, :last_sign_in_at, :timestamp
-    change_column :users, :purchased_at, :time
+    remove_column :users, :current_sign_in_at
+    add_column :users, :current_sign_in_at, :timestamp
+    add_column :users, :current_sign_in_at, :timestamp
+    remove_column :users, :last_sign_in_at
+    add_column :users, :last_sign_in_at, :timestamp
+    add_column :users, :purchased_at, :time
     add_column:users, :sub_start, :string
   end
 end
