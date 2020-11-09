00af9f03568106b1f7ab62167cad45b746795269
Arata <newbluelight1031@gmail.com>
2020-11-09 14:41:48 -0800

    change timestamps to datetime

--- db/migrate/20201026043038_change_times_to_users.rb
+++ db/migrate/20201026043038_change_times_to_users.rb
@@ -1,10 +1,9 @@
 class ChangeTimesToUsers < ActiveRecord::Migration[6.0]
   def change
     remove_column :users, :current_sign_in_at
-    add_column :users, :current_sign_in_at, :timestamp
-    add_column :users, :current_sign_in_at, :timestamp
+    add_column :users, :current_sign_in_at, :datetime
     remove_column :users, :last_sign_in_at
-    add_column :users, :last_sign_in_at, :timestamp
+    add_column :users, :last_sign_in_at, :datetime
     add_column :users, :purchased_at, :time
     add_column:users, :sub_start, :string
   end
