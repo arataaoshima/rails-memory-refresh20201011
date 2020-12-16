1db61dc7f786d58fa06af3dfb26f453eacbd7e71
unknown <newbluelight1031@gmail.com>
2020-12-15 18:53:12 -0800

    fix migration for heroku deploy

--- db/migrate/20201011234935_add_order_to_lectures.rb
+++ db/migrate/20201011234935_add_order_to_lectures.rb
@@ -1,6 +1,7 @@
 class AddOrderToLectures < ActiveRecord::Migration[6.0]
   def change
     add_column :lectures, :order, :integer
-    add_column :lectures, :categtory_id, :integer
+    add_column :lectures, :category_id, :integer
+    add_column :lectures, :order, :integer
   end
 end
--- db/migrate/20201012002446_rename_category_id_to_lectures.rb
+++ db/migrate/20201012002446_rename_category_id_to_lectures.rb
@@ -1,5 +1,5 @@
 class RenameCategoryIdToLectures < ActiveRecord::Migration[6.0]
   def change
-    rename_column :lectures, :categtory_id, :timestamp
+    rename_column :lectures, :categtory_id, :course_id
   end
 end
