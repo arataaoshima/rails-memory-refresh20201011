3b68d5691728e0be9ec6b27e1c8b930ae13586b5
unknown <newbluelight1031@gmail.com>
2020-12-15 22:03:41 -0800

    fix error

--- db/migrate/20201011234935_add_order_to_lectures.rb
+++ db/migrate/20201011234935_add_order_to_lectures.rb
@@ -2,6 +2,5 @@ class AddOrderToLectures < ActiveRecord::Migration[6.0]
   def change
     add_column :lectures, :order, :integer
     add_column :lectures, :category_id, :integer
-    add_column :lectures, :order, :integer
   end
 end
