87cd47c6c799b6a165896de10e0b671a8c80edda
unknown <newbluelight1031@gmail.com>
2021-05-25 17:27:43 -0700

    error bug fix & UI

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,9 @@ class ApplicationController < ActionController::Base
     rescue_from ActionController::RoutingError,   with: :_render_404
   end
 
+  def routing_error
+   raise ActionController::RoutingError, params[:path]
+  end
 
   protected
 
@@ -24,24 +27,25 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
       categories_path
   end
-  def _render_404(e = nil)
+
+  def render_404(e = nil)
   logger.info "Rendering 404 with exception: #{e.message}" if e
 
-  if request.format.to_sym == :json
-    render json: { error: '404 error' }, status: :not_found
-  else
-    render 'errors/404', status: :not_found
+    if request.format.to_sym == :json
+      render json: { error: '404 error' }, status: :not_found
+    else
+      render 'errors/error_404', status: :not_found
+    end
   end
-end
 
-  def _render_500(e = nil)
+  def render_500(e = nil)
     logger.error "Rendering 500 with exception: #{e.message}" if e
     Airbrake.notify(e) if e # Airbrake/Errbitを使う場合はこちら
 
     if request.format.to_sym == :json
       render json: { error: '500 error' }, status: :internal_server_error
     else
-      render 'errors/500', status: :internal_server_error
+      render 'errors/error_500', status: :internal_server_error
     end
   end
 
