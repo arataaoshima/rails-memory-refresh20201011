217d1af481a0ea3d62815eb9d57a72306e402c27
unknown <newbluelight1031@gmail.com>
2021-05-25 18:29:37 -0700

    404

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,11 +6,11 @@ class ApplicationController < ActionController::Base
   rescue_from ActiveRecord::RecordNotFound, with: :render_404
   rescue_from ActionController::RoutingError, with: :render_404
 
-  unless Rails.env.development?
-    rescue_from Exception,                        with: :_render_500
-    rescue_from ActiveRecord::RecordNotFound,     with: :_render_404
-    rescue_from ActionController::RoutingError,   with: :_render_404
-  end
+  # unless Rails.env.development?
+  #   rescue_from Exception,                        with: :_render_500
+  #   rescue_from ActiveRecord::RecordNotFound,     with: :_render_404
+  #   rescue_from ActionController::RoutingError,   with: :_render_404
+  # end
 
   def routing_error
    raise ActionController::RoutingError, params[:path]
